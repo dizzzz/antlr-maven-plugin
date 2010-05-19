082ea4225de4ed30819f834839e779eb79beeb2f
Torsten Werner <twerner@debian.org>
2010-05-19 22:30:21 +0200

    2.1-1 (patches unapplied)
    Imported using git-ubuntu import.

---
+++ debian/orig-tar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+VERSION=$2
+TAR=../antlr-maven-plugin_$VERSION.orig.tar.gz
+DIR=antlr-maven-plugin-$VERSION
+TAG=$(echo "antlr-maven-plugin-$VERSION" | sed -re's/~(alpha|beta)/-\1-/')
+
+svn export https://svn.codehaus.org/mojo/tags/${TAG}/ $DIR
+GZIP=--best tar -c -z -f $TAR --exclude '*.jar' --exclude '*.class' $DIR
+rm -rf $DIR ../$TAG
+
+# move to directory 'tarballs'
+if [ -r .svn/deb-layout ]; then
+  . .svn/deb-layout
+  mv $TAR $origDir && echo "moved $TAR to $origDir"
+fi
